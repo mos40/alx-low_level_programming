d44bb540bf5b92dec25f729deb2a3597eba2fe9d
Mos40 <mosesmkhonza@outlook.com>
2023-08-31 14:45:22 -0700

    I forgot to writr prototype

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,4 +6,6 @@ int clear_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
 #endif
