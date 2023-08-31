13251fd3858668110cf6c2ed1758766cfb2bd050
Mos40 <mosesmkhonza@outlook.com>
2023-08-31 14:41:51 -0700

    Find the password for this program

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+int get_bit(unsigned long int n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
