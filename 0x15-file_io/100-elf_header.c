5e68ffccd23aac8e99b275b217993b549e7b097d
Moses <mosesmkhonza@outlook.com>
2023-10-30 15:49:35 -0700

    elf_header file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -278,10 +278,10 @@ void close_elf(int elf)
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *header;
-	int o, r;
+	int observe, red;
 
-	o = open(argv[1], O_RDONLY);
-	if (o == -1)
+	observe = open(argv[1], O_RDONLY);
+	if (observe == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
@@ -289,15 +289,15 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	header = malloc(sizeof(Elf64_Ehdr));
 	if (header == NULL)
 	{
-		close_elf(o);
+		close_elf(observe);
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
 	}
-	r = read(o, header, sizeof(Elf64_Ehdr));
-	if (r == -1)
+	red = read(observe, header, sizeof(Elf64_Ehdr));
+	if (red == -1)
 	{
 		free(header);
-		close_elf(o);
+		close_elf(observe);
 		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
 		exit(98);
 	}
@@ -314,6 +314,6 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_entry(header->e_entry, header->e_ident);
 
 	free(header);
-	close_elf(o);
+	close_elf(observe);
 	return (0);
 }
